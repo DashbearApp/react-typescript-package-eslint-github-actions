ffb522f52b457c22a828989e63996513bdb1ff30
Sar Malik <malik@sarthak.ca>
2021-04-07 20:06:23 -0400

    example:refactor, conformity to eslint

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import * as React from 'react';
 
-import { ExampleComponent } from 'react-ts-package-eslint-github-actions'
-import 'react-ts-package-eslint-github-actions/dist/index.css'
+import { ExampleComponent } from 'react-ts-package-eslint-github-actions';
+import 'react-ts-package-eslint-github-actions/dist/index.css';
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+class App extends React.Component {
+    public render(): React.ReactElement {
+        return <ExampleComponent text='Create React Library Example 😄' />;
+    }
 }
 
-export default App
+export default App;
